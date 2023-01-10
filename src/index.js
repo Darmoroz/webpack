03b2f5f5cc8bd061dc0bad17aca5a0f43d6173dd
Olexandr Darmoroz <darmoroz99@gmail.com>
2023-01-10 09:54:34 +0200

    webpack + webpack-cli

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const fred = 'Ivan';
+console.log(fred);
+console.log(`My name ${fred}`);
+console.log(333);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	// mode: 'development',
+	// entry: './src/index.js',
+	// output: {
+	// 	filename: 'index.html',
+	// 	path: path.resolve(__dirname, 'dist'),
+	// },
+};
